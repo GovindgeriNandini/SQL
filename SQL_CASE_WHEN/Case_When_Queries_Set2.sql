4ca22a3e3699199ac10965e5e61604eaf5534c30
GovindgeriNandini <nandinig2646@gmail.com>
2026-04-18 11:01:16 +0530

    Added a query

--- SQL_CASE_WHEN/Case_When_Queries_Set2.sql
+++ SQL_CASE_WHEN/Case_When_Queries_Set2.sql
@@ -1,4 +1,4 @@
-Q1. Count how many employees fall in each salary band (High / Mid / Low). Show salary_band and count.
+--Q1. Count how many employees fall in each salary band (High / Mid / Low). Show salary_band and count.
 
 SELECT
 CASE
@@ -15,7 +15,15 @@ GROUP BY CASE
 END;
 
 
-Q2. For each order, add a bonus_points column: Laptop = 500 pts, Phone = 200 pts, Tablet = 300 pts, anything else = 100 pts.
+--Q2. For each order, add a bonus_points column: Laptop = 500 pts, Phone = 200 pts, Tablet = 300 pts, anything else = 100 pts.
+SELECT Product,Amount, 
+CASE 
+	WHEN Product = 'Laptop' THEN 500
+	WHEN Product = 'Phone' THEN 200
+	WHEN Product = 'Tablet' THEN 300
+	ELSE 100
+END AS Bonus_Points
+FROM Orders;
 
 Q3. Show each employee's name, department, and a tax_bracket based on salary. Then filter to show only employees in the 'High' bracket.
 
