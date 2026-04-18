c21a4988173d940e41a9899fb8572c2b871e4431
GovindgeriNandini <nandinig2646@gmail.com>
2026-04-18 12:08:26 +0530

    Added new query

--- SQL_CASE_WHEN/Case_When_Queries_Set3.sql
+++ SQL_CASE_WHEN/Case_When_Queries_Set3.sql
@@ -8,3 +8,19 @@ CASE
 	ELSE 'Senior' 
 END AS age_group
 FROM patients;
+
+
+--Q2. Show each doctor's experience tier: less than 5 years as "Junior", 5–14 years as "Mid-Level", 
+		--and 15 or more as "Senior". Display doctor name, experience, and tier.
+
+SELECT full_name, experience_yrs,
+CASE
+	WHEN experience_yrs < 5 THEN 'Junior'
+	WHEN experience_yrs BETWEEN 5 AND 14 THEN 'Mid-Level'
+	ELSE 'Senior'
+END AS experience_tier
+FROM doctors;
+
+
+
+
