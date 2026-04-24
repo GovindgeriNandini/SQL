68fbfc0bb8144e122786f9830f3da8f280bb242e
GovindgeriNandini <nandinig2646@gmail.com>
2026-04-24 15:21:16 +0530

    Added query 11

---
+++ SQL_CASE_WHEN/Case_When_Queries_Set4.sql
@@ -0,0 +1,13 @@
+-- Medium
+-- Q11. For each patient, calculate their total bill amount after discount. 
+--          Then classify the final amount: below 2000 as "Low Bill", 2000–5000 as "Medium Bill", above 5000 as "High Bill".
+--          Show patient name and bill category. (Join billing with patients)
+
+SELECT p.full_name,
+ROUND((b.consultation_fee + b. medicine_cost + b.test_charges) * ( 1 - b.discount/100.0), 2) AS Total_Fee_After_Discount,
+CASE 
+	WHEN (b.consultation_fee + b. medicine_cost + b.test_charges) * ( 1 - b.discount/100.0)  < 2000 THEN 'Low'
+	WHEN (b.consultation_fee + b. medicine_cost + b.test_charges) * ( 1 - b.discount/100.0) BETWEEN 2000 AND 5000 THEN 'Medium'
+	ELSE 'High'
+END AS Bill_Category
+FROM Patients p LEFT JOIN Billing b ON p.patient_id = b.patient_id;
