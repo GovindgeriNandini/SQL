ff1018f4c6dc62782969002352efd9640c2342bd
GovindgeriNandini <nandinig2646@gmail.com>
2026-04-24 15:51:49 +0530

    Added query 13

--- SQL_CASE_WHEN/Case_When_Queries_Set4.sql
+++ SQL_CASE_WHEN/Case_When_Queries_Set4.sql
@@ -1,7 +1,7 @@
 -- Medium
 -- Q11. For each patient, calculate their total bill amount after discount. 
---          Then classify the final amount: below 2000 as "Low Bill", 2000–5000 as "Medium Bill", above 5000 as "High Bill".
---          Show patient name and bill category. (Join billing with patients)
+--      Then classify the final amount: below 2000 as "Low Bill", 2000–5000 as "Medium Bill", above 5000 as "High Bill".
+--      Show patient name and bill category. (Join billing with patients)
 
 SELECT p.full_name,
 ROUND((b.consultation_fee + b. medicine_cost + b.test_charges) * ( 1 - b.discount/100.0), 2) AS Total_Fee_After_Discount,
@@ -15,7 +15,7 @@ FROM Patients p LEFT JOIN Billing b ON p.patient_id = b.patient_id;
 
 
 --Q12. List each doctor with their department name and classify department budget: below 700000 as "Low Budget", 
--- 		700000–1000000 as "Medium Budget", above 1000000 as "High Budget". Show doctor name, dept name, and budget category.
+-- 	   700000–1000000 as "Medium Budget", above 1000000 as "High Budget". Show doctor name, dept name, and budget category.
 
 SELECT doc.full_name, dep.dept_name, dep.budget,
 CASE
@@ -25,6 +25,18 @@ CASE
 END AS Budget_Category
 FROM Doctors doc LEFT JOIN Departments dep  ON doc.dept_id = dep.dept_id;
 
+
+-- Q13. For each admission, calculate the number of days the patient stayed. 
+--      Classify stays: 1–3 days as "Short Stay", 4–7 days as "Week Stay", above 7 days as "Long Stay". 
+--      Show patient_id, days_stayed, and stay_category.
+
+SELECT patient_id, (discharge_date - admit_date) AS Days_Stayed,
+CASE 
+	WHEN (discharge_date - admit_date) BETWEEN 1 AND 3 THEN 'Short Stay'
+	WHEN (discharge_date - admit_date) BETWEEN 4 AND 7 THEN 'Week Stay'
+	ELSE 'Long Stay'
+END AS Stay_Category
+FROM ward_admissions;
 
 
 
